3b4e97027217589f61071a99fd2a26a53bc3428e
Parviz Babakishiyev <40580243+parvizbabakishiyev@users.noreply.github.com>
2023-07-19 13:45:11 +0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={classes.footer}>
       <span className={classes.footer__copyright}>
-        {new Date().getFullYear()} &#169; All rights reserved. Developed by <a href='https://github.com/parvizbabakishiyev'>Parviz Babakishiyev</a>
+        {new Date().getFullYear()} &#169; All rights reserved.<br/>Developed by <a href='https://github.com/parvizbabakishiyev'>Parviz Babakishiyev</a>
       </span>
     </footer>
   );
